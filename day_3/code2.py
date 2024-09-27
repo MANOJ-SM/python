709c5ebc74e50a78aa22295e3262fc30b38cd2e6
MANOJ-SM <157309036+MANOJ-SM@users.noreply.github.com>
2024-09-27 05:13:17 +0000

    added for in prgm

--- day_3/code2.py
+++ day_3/code2.py
@@ -1,4 +1,25 @@
-fruits = [ "apple", "banana","Guava"]
+# fruits = [ "apple", "banana","Guava"]
 
-for fruit in fruits :
-    print(fruit);
+# for fruit in fruits :
+#     print(fruit)
+
+
+# ------------------------------------------------------------
+
+
+# count = 1
+
+# while count <=5:
+#     print(count)
+#     count += 1
+
+
+# -----------------------------------------------
+
+# numbers = [ 1,2,3,4,5,6]
+
+# for number in numbers:
+#     if number % 2==0:
+#         print(f"{number} is even")
+#     else :
+#         print(f"{number} is odd")    
