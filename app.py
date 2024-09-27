104e389c6ffd553fd28c0285ac75daa94d8ba59c
MANOJ_SM <manumedleri@gmail.com>
2024-09-27 10:06:11 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# print("hello world");
+
+# name = input("what is ur name : ");
+# print("hello ," + name + "!");
+
+
+num1 = int(input("enter the first no :"))
+num2 = int(input("enter the first no :"))
+
+sum = num1 + num2
+print("the sum is :",sum)
+
