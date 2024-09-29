36535b8609b911e9d6bf4e5c8f96be999bab0973
MANOJ-SM <157309036+MANOJ-SM@users.noreply.github.com>
2024-09-29 03:44:34 +0000

    added prgms

---
+++ day_4/functions.py
@@ -0,0 +1,48 @@
+# def greet(name):
+#     print(f"namaste! {name} ")
+
+# greet("manu")
+
+#--------------------------------------------------------------     
+
+# def add_numbers(a,b):
+#     return a + b
+
+# result = add_numbers(2,3)
+# print(result)
+
+# --------------------------------------------------------------
+
+# def my_function():
+#     x=10  #local variable 
+#     print(x)
+
+# my_function()    
+# print(x)              #NameError: name 'x' is not defined
+
+# ----------------------------------------------------------------
+
+
+# x = 29
+# y = 30
+# def my_function():
+#     z =10
+#     print(x)
+
+
+# my_function()
+# print(x)
+# print(y)
+# print(z)   # z is not defined 
+
+#-------------------------------------------------------------------
+
+def calculate_area(length,width):
+    return length * width
+
+area1 = calculate_area(2,4)
+area2 = calculate_area(12,4)
+
+print(area1)
+print(area2)
+print(area1,area2)
