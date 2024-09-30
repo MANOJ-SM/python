96975648872f745472c5efab75c4ded503c50a47
MANOJ-SM <157309036+MANOJ-SM@users.noreply.github.com>
2024-09-30 14:22:02 +0000

    modified data_type tuple

--- day_5/data_types.py
+++ day_5/data_types.py
@@ -6,11 +6,11 @@
 
 # 2) Tuple   # immutable   # ordered  #Allow duplicates 
 
-my_tuple = ( 1,2,3, "apple","banana",3);
-print(len(my_tuple));  
+# my_tuple = ( 1,2,3, "apple","banana",3);
+# print(len(my_tuple));  
              
-my_tuple.(4,5,6, "guava","orange",3);
-print(my_tuple);
+# my_tuple.(4,5,6, "guava","orange",3);
+# print(my_tuple);
 
 
 # 3) Dictinary   #  mutable   #unordered  #unique key 
