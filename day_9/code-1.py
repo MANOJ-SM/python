bd821d66a352c20f347744d5108939ed17a064f5
MANOJ-SM <157309036+MANOJ-SM@users.noreply.github.com>
2024-10-07 04:36:18 +0000

    added exception prblm

---
+++ day_9/code-1.py
@@ -0,0 +1,18 @@
+
+# Exception
+
+# def check_positive(number):
+#     if number < 0:
+#         raise ValueError("Negative numbers are not Allowed!")
+    
+
+# try:
+#     print(check_positive(5)) 
+# except ValueError as e:
+#     print(e)
+
+
+# try :
+#     result = 10/0
+# except ZeroDivisionError:
+#     print("oops! you cant divide by zero,") 
