425e7347b748c1be195e5b59d46a74e4d0179216
MANOJ-SM <157309036+MANOJ-SM@users.noreply.github.com>
2024-10-07 04:58:53 +0000

    added one more prblm

--- day_9/code-1.py
+++ day_9/code-1.py
@@ -16,3 +16,34 @@
 #     result = 10/0
 # except ZeroDivisionError:
 #     print("oops! you cant divide by zero,") 
+
+# finally-clause
+
+# try :
+#     file = open("example.txt","r")
+#     # some kind of code raise an exception 
+
+# except FileNotFoundError:
+#     print("file not found!")
+
+# finally:
+#     file.close()
+#     print("file closed")
+
+# try:
+#     num1 = float(input("enter the 1st number: "))
+#     num2 = float(input("enter the 2nd number: "))
+#     result = num1 /num2
+#     print(f"the result is : {result}")
+
+# except FileNotFoundError:
+#     print("File not found!")
+
+# except ZeroDivisionError:
+#    print("oops! you cant divide by zero,")      
+
+
+ 
+
+
+
